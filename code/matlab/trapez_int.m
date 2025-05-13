480843182fbaae81cfd762c69d06a68e80db4813
Luca Seggiani <seggianiluca2004@gmail.com>
2025-05-13 15:37:49 +0200

    Checkup

--- code/matlab/trapez_int.m
+++ code/matlab/trapez_int.m
@@ -1,29 +1,35 @@
-function j = trapez_int(f, a, b, L)
+function j = trapez_int(f, a, b, L, print)
+    if nargin < 5 
+        print = false 
+    end
+
     xi = linspace(a, b, L + 1)';
     yi = f(xi);
 
-    % stampa
-    clf;
-    hold on;
-
-    % funzione
-    fplot(f, [a, b], 'r');
-
-    % punti
-    plot(xi, yi, 'ob');
-
-    % rette
-    for i = 1:L
-        x0 = xi(i); x1 = xi(i + 1);
-        y0 = yi(i); y1 = yi(i + 1);
-
-        m = (y1 - y0) / (x1 - x0);
-        r = @(x) m * (x - x0) + y0;
-        
-        fplot(r, [x0, x1], 'b')
+    if print
+        % stampa
+        clf;
+        hold on;
+    
+        % funzione
+        fplot(f, [a, b], 'r');
+    
+        % punti
+        plot(xi, yi, 'ob');
+    
+        % rette
+        for i = 1:L
+            x0 = xi(i); x1 = xi(i + 1);
+            y0 = yi(i); y1 = yi(i + 1);
+    
+            m = (y1 - y0) / (x1 - x0);
+            r = @(x) m * (x - x0) + y0;
+            
+            fplot(r, [x0, x1], 'b')
+        end
+    
+        hold off;
     end
-
-    hold off;
 
     % integra
     j = yi(1) + yi(L + 1) + 2 * sum(yi(2:L)); 
